b9fdd58c08f08c614a0631ef7e6e9a48d3d39f7e
Zuzhao Ye <zuzhaoye@foxmail.com>
2022-11-15 12:59:51 -0800

    Add files via upload

---
+++ lo_step.py
@@ -0,0 +1,89 @@
+from math import sqrt, inf
+from settings import *
+
+''''''
+def get_objective(x, Pnet, Q, DT, V, pE, pF, pD, M):
+    
+    PG = Pnet-x
+    QU = Q*(x+(1-sqrt(K))*abs(x))*DT
+    Vg = V*(max(PG,0)*pE*DT + min(PG,0)*pF*DT + max(0,PG-M)*pD*WEIGHT_DEMAND_CHARGE)
+    obj = QU + Vg
+    
+    return obj
+
+def clip(value, lower, upper):
+    return lower if value < lower else upper if value > upper else value
+
+'''
+Perform Lyapunov optimization for a single time step.
+Inputs:
+    - PS: Solar generation power (kW) at current time step (PS > 0 indicates positive power generation)
+    - LB: Building load (kW) at current time step
+    - Ppeak: Historical peak load (kW)
+    - SoC: BESS SoC at current time step
+    - pE: Electricity price ($/kWh) at current time step (buying from the grid)
+    - pF: Electricity price ($/kWh) at current time step (selling to the grid)
+    - pD: Demand charge price ($/kW) at current time step
+    - V: The balance term between immediate costs and energy reservation
+    - DT: Control time interval (hour)
+    
+Outputs:
+    - x: BESS discharge power (x>0, BESS discharges to offset the building load, and x<0 BESS get charged)
+'''
+
+def lo_step(LB, PS, Ppeak, SoC, pE, pF, pD, V, DT):
+
+    Pnet = LB - PS # Calculate net load
+    
+    if Ppeak > P_PEAK_TARGET: # Determine M based on if the historical peak is above the targeted peak
+        M = Ppeak
+    else:
+        M = P_PEAK_TARGET
+            
+    # Get current BESS energy queue
+    Q = E_MAX - SoC*E_FULL - DT*P_MAX*sqrt(K)
+    
+    # Get the objective value at p_BESS = P_MIN
+    obj_xmin = get_objective(P_MIN, Pnet, Q, DT, V, pE, pF, pD, M)
+    
+    # Get the objective value at p_BESS = P_MAX
+    obj_xmax = get_objective(P_MAX, Pnet, Q, DT, V, pE, pF, pD, M)
+    
+    # Get the objective value at p_BESS = 0
+    obj_x0 = get_objective(0, Pnet, Q, DT, V, pE, pF, pD, M)
+    
+    # Get the objective value at p_BESS = Pnet
+    if Pnet >= P_MIN and Pnet <= P_MAX:
+        obj_xnet = get_objective(Pnet, Pnet, Q, DT, V, pE, pF, pD, M)
+    else:
+        obj_xnet = inf
+    
+    # Get the objective value at p_BESS = Pnet-M
+    if Pnet-M >= P_MIN and Pnet-M <= P_MAX:
+        obj_xnetm = get_objective(Pnet-M, Pnet, Q, DT, V, pE, pF, pD, M)
+    else:
+        obj_xnetm = inf
+
+    objList = [obj_xmin, obj_xmax, obj_x0, obj_xnet, obj_xnetm]
+    xList = [P_MIN, P_MAX, 0, Pnet, Pnet-M]
+    objMin = min(objList)
+    minIdx = objList.index(objMin)
+    x = xList[minIdx]
+    
+    # If the command is charging, check if charging is allowed
+    # Charging BESS is only allowed when there is surplus of solar
+    if x < 0:
+        if PS < 0:
+            x_max = abs(PS)
+        else:
+            x_max = 0
+        x = min(x_max, x)
+        
+    # clip Q value based on max and min values
+    tmpQ = Q + x*DT + abs(x)*DT*(1-sqrt(K))
+    if tmpQ > Q_MAX:
+        x = (Q_MAX - Q)/((2-sqrt(K))*DT)
+    elif tmpQ < Q_MIN:
+        x = (Q_MIN - Q)/(sqrt(K)*DT)
+    
+    return x
---
+++ settings.py
@@ -0,0 +1,130 @@
+from math import sqrt
+
+'''Settings'''
+
+K = 0.88 # BESS energy efficiency (or round-trip efficiency)
+E_FULL = 336  # BESS full capacity (kWh)
+E_MAX = 0.9*E_FULL # BESS maximum allowable capacity (90% of full)
+E_MIN = 0.15*E_FULL # BESS maximum allowable capacity (90% of full)
+P_MAX = 57 # BESS maximum allowable power flow (being discharging)
+P_MIN = -57 # BESS minimum allowable power flow (being charging)
+DT = 1/3600 # Control time interval, in hour
+Q_MAX = E_MAX - E_MIN - DT*P_MAX*sqrt(K) # The maximum value of the BESS energy queue 
+Q_MIN = -DT*P_MAX*sqrt(K) # The miminum value of the BESS energy queue 
+P_PEAK_TARGET = 20 # Target peak load (kW)
+WEIGHT_DEMAND_CHARGE = 1 # The weight of demand charge in Eq.(26b)
+SELL_BACK_ENABLED = True # If allowing sending electricity back to the grid
+if SELL_BACK_ENABLED:
+    P_MIN_SELL_BACK = float('-inf')
+else:
+    P_MIN_SELL_BACK = 0
+SELL_BACK_DISCOUNT = 1 # The price discount of sell-back electricity. Examples: For net-metering, use 1. If the sell-back price is 90% of the original price, use 0.9
+month_days = {1: 31, 2:28, 3:31, 4:30, 5:31, 6:30, 7:31, 8:31, 9:30, 10:31, 11:30, 12:31}
+
+peakNames = {0: "Super Off-Peak",
+             1: "Off-Peak",
+             2: "On-Peak"}
+peakHourTable_weekday = {0: 0,
+             1: 0,
+             2: 0,
+             3: 0,
+             4: 0,
+             5: 0,
+             6: 1,
+             7: 1,
+             8: 1,
+             9: 1,
+             10: 1,
+             11: 1,
+             12: 1,
+             13: 1,
+             14: 1,
+             15: 1,
+             16: 2,
+             17: 2,
+             18: 2,
+             19: 2,
+             20: 2,
+             21: 1,
+             22: 1,
+             23: 1}
+peakHourTable_weekdayMarApr = {0: 0,
+             1: 0,
+             2: 0,
+             3: 0,
+             4: 0,
+             5: 0,
+             6: 1,
+             7: 1,
+             8: 1,
+             9: 1,
+             10: 0,
+             11: 0,
+             12: 0,
+             13: 0,
+             14: 1,
+             15: 1,
+             16: 2,
+             17: 2,
+             18: 2,
+             19: 2,
+             20: 2,
+             21: 1,
+             22: 1,
+             23: 1}
+peakHourTable_weekend = {0: 0,
+             1: 0,
+             2: 0,
+             3: 0,
+             4: 0,
+             5: 0,
+             6: 0,
+             7: 0,
+             8: 0,
+             9: 0,
+             10: 0,
+             11: 0,
+             12: 0,
+             13: 0,
+             14: 1,
+             15: 1,
+             16: 2,
+             17: 2,
+             18: 2,
+             19: 2,
+             20: 2,
+             21: 1,
+             22: 1,
+             23: 1}
+summerMonths = [6, 7, 8, 9, 10]
+winterMonths = [1, 2, 3, 4, 5, 11, 12]
+
+# Energy charge
+energyChargeRef_Winter = {0: 0.09432,
+                          1: 0.12207,
+                          2: 0.21780}
+energyChargeRef_Summer = {0: 0.09960,
+                          1: 0.10423,
+                          2: 0.17868}
+
+# Demand charge
+cd_OnPeak_Winter = 25.56
+cd_OnPeak_Summer = 27.80
+cd_NonCoincident_Winter = 29.89
+cd_NonCoincident_Summer = 31.32
+cd_Generation_Winter = 0.0
+cd_Generation_Summer = 12.18
+demandChargeRef_Winter = {0: cd_NonCoincident_Winter + cd_Generation_Winter,
+                          1: cd_NonCoincident_Winter + cd_Generation_Winter,
+                          2: cd_NonCoincident_Winter + cd_Generation_Winter + cd_OnPeak_Winter}
+demandChargeRef_Summer = {0: cd_NonCoincident_Summer + cd_Generation_Summer,\
+                          1: cd_NonCoincident_Summer + cd_Generation_Summer,\
+                          2: cd_NonCoincident_Summer + cd_Generation_Summer + cd_OnPeak_Summer}
+
+# V values
+V_NONPEAK = 16.237767391887218
+V_PEAK = 1080.0597388923065
+VsTable = {0: V_NONPEAK,
+           1: V_NONPEAK,
+           2: V_PEAK} 
+
